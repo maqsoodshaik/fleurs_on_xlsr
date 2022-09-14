4ce479070f64dc2967f53e51f23facac9e8dd843
maqsoodshaik <shaikmdmaqsood@gmail.com>
2022-09-14 17:05:29 +0200

    timit analysis with dictionary as output

---
+++ timit_analysis.py
@@ -0,0 +1,101 @@
+import librosa
+import torchaudio
+from transformers import AutoFeatureExtractor, Wav2Vec2ForPreTraining
+import torch
+import librosa.display
+import os
+from collections import Counter
+import pickle
+
+feature_extractor = AutoFeatureExtractor.from_pretrained("facebook/wav2vec2-large-xlsr-53")
+model = Wav2Vec2ForPreTraining.from_pretrained("facebook/wav2vec2-large-xlsr-53")
+def xlsr_codes(audio_path):
+    wav,sample = torchaudio.load(audio_path)
+    input_values = feature_extractor(wav.squeeze(), return_tensors="pt",sampling_rate = 16000).input_values
+    with torch.no_grad():
+        codevector_probs,codevectors,codevector_idx = model(input_values)
+    output = codevector_idx.view(-1,2)[:,0].tolist()
+    return output
+def codes_low_high(audio_path,xlsr_codes):
+    y, sr = librosa.load(audio_path)
+    tot_samp = librosa.time_to_samples(librosa.get_duration(y=y, sr=sr),sr=16000)
+    low = 0,
+    high = 0
+    time = 0
+    time_1 = 0.025
+    low_lst =[]
+    high_lst =[]
+    # print(len(xlsr_codes))
+    for i,k in enumerate(xlsr_codes):
+        low,high = librosa.time_to_samples(time,sr = 16000),librosa.time_to_samples(time_1,sr = 16000)
+        low_lst.append(int(low))
+        high_lst.append(int(high))
+        # print(f'{low}-{high}:{i}')
+        # if i ==0:
+        #     time += 0.025
+        #     time_1 += 0.025
+        # else:
+        time += 0.020
+        time_1 += 0.020
+        # print(low_lst)
+    if high<tot_samp:
+        # low_lst.append(tot_samp-librosa.time_to_samples(0.025,sr=16000))
+        low_lst.append(tot_samp)
+        high_lst.append(tot_samp)
+    return low_lst,high_lst
+def val_ret(lst,val):
+    return lst[next(x[0]-1 for x in enumerate(lst) if x[1] > val)]
+def phn_ind(phn_path,low_lst,high_lst,xlsr_codes,phn_dict ={}):
+    with open(phn_path) as f:
+        lines,lines_1,phn = zip(*[(line.split()) for line in f])
+    # plt.figure()
+    # librosa.display.waveshow(y, sr=sr )
+    # plt.show()
+    # fig, ax = plt.subplots()
+    # librosa.display.waveshow(y, sr=sr )
+    # ax.text(x=0, y=1.0, s="|".join([s for s in phn]), transform=ax.transAxes, fontsize=20)
+    # plt.show()
+    for val,p in enumerate(phn):
+        l = val_ret(low_lst,int(lines[val]))
+        if val < len(phn)-1:
+            h = val_ret(high_lst,int(lines_1[val]))
+        else:
+            h = high_lst[-1]
+        if p in phn_dict:
+            phn_dict[p] += xlsr_codes[low_lst.index(l):high_lst.index(h)+1]
+        else:
+            phn_dict[p] = xlsr_codes[low_lst.index(l):high_lst.index(h)+1]
+        print(f'{lines[val]}-{lines_1[val]}_{p}:{xlsr_codes[low_lst.index(l):high_lst.index(h)+1]}')
+
+    return phn_dict
+
+rootdir = '/Users/mohammedmaqsoodshaik/Desktop/hiwi/task1/TIMIT/timit/'
+phn_dict ={}
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        if 'wav'in os.path.join(subdir, file):
+            print(os.path.join(subdir, file))
+            audio_path = os.path.join(subdir, file)
+            phnpath = audio_path.replace('wav','phn')
+            print(phnpath)
+            output = xlsr_codes(audio_path)
+            low_lst,high_lst = codes_low_high(audio_path,output)
+            phn_dict = phn_ind(phnpath,low_lst,high_lst,output,phn_dict)
+with open('saved_dictionary_codebook_1.pkl', 'wb') as f:
+    pickle.dump(phn_dict, f)
+for dict_val in phn_dict.keys():
+    print(f'{dict_val}-{dict(Counter(phn_dict[dict_val]))}')
+with open("saved_dictionary_codebook_1.pkl", "rb") as f:
+    phn_dict = pickle.load(f)
+import operator
+from collections import OrderedDict
+
+for dict_val in phn_dict.keys():
+    dict1 = dict(Counter(phn_dict[dict_val]))
+    sorted_tuples = sorted(dict1.items(), key=operator.itemgetter(1))
+    # print(sorted_tuples)  # [(1, 1), (3, 4), (2, 9)]
+
+    sorted_dict = OrderedDict()
+    for k, v in sorted_tuples:
+        sorted_dict[k] = v
+    print(f"{dict_val}-{dict(sorted_dict).popitem()[0]}")
