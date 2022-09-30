d6f6d97b301ba125581847ca5391979db212608b
maqsoodshaik <shaikmdmaqsood@gmail.com>
2022-09-30 09:33:24 +0200

    Male vs female analysis

---
+++ male_female_timit_analysis.py
@@ -0,0 +1,39 @@
+import os
+import pickle
+
+from sim_phonemes import similarity_calculation,phn_dict_generator
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    codebook = 1
+    folder_name = "timit_pkl_only_english"
+    pickle_path = (
+        f"/Users/mohammedmaqsoodshaik/Desktop/hiwi/task1/{folder_name}/codebook{codebook}/"
+    )
+    rootdir = "/Users/mohammedmaqsoodshaik/Desktop/hiwi/task1/TIMIT/timit/"
+
+    
+
+    phn_dict_m = {}
+    starts_with = "m"
+    phn_dict_m = phn_dict_generator(phn_dict_m,pickle_path,rootdir, starts_with)
+    phn_dict_f = {}
+    starts_with = "f"
+    phn_dict_f = phn_dict_generator(phn_dict_f,pickle_path,rootdir, starts_with)
+    abs_discount = 0.00000000002
+    sim_mt = similarity_calculation(phn_dict_m, phn_dict_f,abs_discount)
+    sim_mt = np.array(sim_mt)
+    sim_mt_diag = sim_mt.diagonal()
+    sim_mt_diag = 1.0 - sim_mt_diag
+    sorted_phn_sim = [x for _, x in sorted(zip(sim_mt_diag, list(phn_dict_m.keys())))]
+    g = sns.barplot(sorted_phn_sim, sorted(sim_mt_diag))
+    g.set_xticklabels(g.get_xticklabels(), rotation=-90)
+    plt.xlabel("phonemes")
+    plt.ylabel("similarity")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
